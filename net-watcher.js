a2a121c9eeb30db9efa2668a9f163128bcbf7597
Sumudu Jayawardana <sumudujay@gmail.com>
2015-02-09 08:32:06 +1100

    Implemented client json watcher

--- net-watcher.js
+++ net-watcher.js
@@ -2,23 +2,30 @@
 const
   fs = require('fs'),
   net = require('net'),
-  filename = process.argv[2];
+  filename = process.argv[2],
 
 server = net.createServer(function(connection) {
-  // reporting
-  console.log('Subscriber connected.');
-  connection.write("Now watching '" + filename + "' for changes...\n");
+    // reporting
+    console.log('Subscriber connected.');
+    connection.write(JSON.stringify({
+                                      type: 'watching',
+                                      file: filename
+                                    }) + '\n');
 
-  // watcher setup
-  let watcher = fs.watch(filename, function() {
-    connection.write("File '" + filename + "' changed: " + Date.now() + "\n");
-  });
+    // watcher setup
+    let watcher = fs.watch(filename, function() {
+      connection.write(JSON.stringify({
+                                        type: 'changed',
+                                        file: filename,
+                                        timestamp: Date.now()
+                                      }) + '\n');
+    });
 
-  // cleanup
-  connection.on('close', function() {
-    console.log('Subscriber disconnected.');
-    watcher.close();
-  });
+    // cleanup
+    connection.on('close', function() {
+      console.log('Subscriber disconnected.');
+      watcher.close();
+    });
 });
 
 if (!filename) {
